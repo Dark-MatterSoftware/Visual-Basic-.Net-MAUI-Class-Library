98e44dca5e80fd88fb08a8a9751bb574086a9e00
Donald Joyce <111136961+Dark-MatterSoftware@users.noreply.github.com>
2022-08-12 10:17:30 -0500

    Initial upload

---
+++ Start.vb
@@ -0,0 +1,20 @@
+﻿Imports Microsoft.Maui.Controls
+
+Namespace MauiLib
+    ' All the code in this file is included in all platforms.
+    Public Class Start
+        Inherits ContentPage
+
+        'Add controls here
+
+        Public Sub New()
+
+            'Specify Controls with customization and then add parent to Me.Content
+
+
+
+            'Me.Content = 'Replace parent control here'
+
+        End Sub
+    End Class
+End Namespace
